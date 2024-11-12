2d5fd5d86da3365b7398a93e0495784d7f6543cb
amarah55-coder <maheryrazafimanantsoa55@gmail.com>
2024-11-12 11:31:02 +0100

    goo

---
+++ javascript/exo1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+let question = prompt("Est-ce que vous avez un compte?");
+const answer= question;
+if (answer == "oui"){
+    console.log("Connectez-vous");
+}else if (answer == "non"){
+    console.log("Veuillez créer un compte");
+}else{
+    console.log("Votre réponse n'est pas valide");
+}
